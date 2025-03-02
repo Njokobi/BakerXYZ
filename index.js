fb9f460a07d736c1605c9190f0aa90ec6fa2943c
Jacoby Adwin <jacoby_adwin@student.waylandps.org>
2025-03-02 13:54:32 -0500

    Add mandatory filter for baked goods/desserts

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let pageTaglist = [];
+let pageTaglist = [`c=dessert`, `c=tart`, `c=sweet`, `c=pudding`, `c=baking`, `c=pancake`];
 let pageMealList = [];
 
-function addToTaglist(tag){
-    if(pageTaglist.includes(tag)){
+function addToTaglist(tag) {
+    if (pageTaglist.includes(tag)) {
         pageTaglist.splice(pageTaglist.indexOf(tag));
     } else {
         pageTaglist.push(tag);
     }
     console.log(pageTaglist);
+}
+
+async function setPageMealList(mealList) {
+    pageMealList = await mealList;
+    console.log(pageMealList);
+}
+
+async function getIdsByPageTaglist() {
+    return await getIdsByTaglist(pageTaglist);
 }
 
 async function getIdsByTaglist(taglist) {
@@ -25,6 +34,7 @@ async function getIdsByTaglist(taglist) {
                 for (j in data.meals) {
                     idList.push(data.meals[j].idMeal)
                 }
+
             })
     }
     return idList;
@@ -45,30 +55,32 @@ async function getRandomMealId() {
 async function getMealById(id) {
     console.log(`Fetching: https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     let meal = await fetch(
-            `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-        ).then((res) => {
-            if (!res.ok) {
-                console.log("Connection Error");
-                return;
-            }
-            return res.json();
-        }).then((data) => {
-            console.log(data);
-            if(data.meals === "Invalid ID"){
-                console.log("Failed to fetch meals from id, perhaps incorrect formatting?");
-            }
-            return data.meals[0];
-        });
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    ).then((res) => {
+        if (!res.ok) {
+            console.log("Connection Error");
+            return;
+        }
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        if (data.meals === "Invalid ID") {
+            console.log("Failed to fetch meals from id, perhaps incorrect formatting?");
+        }
+        return data.meals[0];
+    });
     return meal;
 }
 
-async function getMealListByIdList(idListPromise){
+async function getMealListByIdList(idListPromise) {
     let tempList = [];
     let idList = await idListPromise;
-    for(i in idList){
+    for (i in idList) {
         await getMealById(idList[i]).then((meal) => {
             tempList.push(meal);
         });
     }
-    return tempList;
+    return tempList.sort((a, b) => {
+        -a.strMeal.toLowerCase().localeCompare(b.strMeal.toLowerCase());
+    });
 }
